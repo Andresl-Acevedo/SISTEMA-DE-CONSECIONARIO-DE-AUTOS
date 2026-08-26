35f5b270860e0ab374598b4407de5093397c4dc9
oscar <oscara-serranos@unilibre.edu.co>
2026-08-26 10:33:30 -0500

    Create sede.java

---
+++ sede.java
@@ -0,0 +1,71 @@
+Clase sede
+public class Sede {
+
+    private String idSede;
+    private String nombre;
+    private String ciudad;
+    private String direccion;
+    private String telefono;
+
+    public Sede(String idSede, String nombre, String ciudad,
+                String direccion, String telefono) {
+        this.idSede = idSede;
+        this.nombre = nombre;
+        this.ciudad = ciudad;
+        this.direccion = direccion;
+        this.telefono = telefono;
+    }
+
+    public void mostrarInformacion() {
+        System.out.println("----- SEDE -----");
+        System.out.println("ID: " + idSede);
+        System.out.println("Nombre: " + nombre);
+        System.out.println("Ciudad: " + ciudad);
+        System.out.println("Direccion: " + direccion);
+        System.out.println("Telefono: " + telefono);
+    }
+
+    public void actualizarTelefono(String nuevoTelefono) {
+        this.telefono = nuevoTelefono;
+    }
+
+    public String getIdSede() {
+        return idSede;
+    }
+
+    public void setIdSede(String idSede) {
+        this.idSede = idSede;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getCiudad() {
+        return ciudad;
+    }
+
+    public void setCiudad(String ciudad) {
+        this.ciudad = ciudad;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+}
